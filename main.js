cd134e07c357e702f79bf7d9fd95eb19d59cdc77
Wesley Jakob <lewesleydj@gmail.com>
2023-04-25 09:32:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('formValores');
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    let aValue = document.querySelector('#aValue');
+    let bValue = document.querySelector('#bValue');
+    const msgSucesso = `O valor <strong>${aValue.value}</strong> é maior que o valor <strong>${bValue.value}</strong>`
+    const msgError = `O valor <strong>${aValue.value}</strong> é menor que o valor <strong>${bValue.value}</strong>`
+    if (aValue.value < bValue.value){
+        const containerMsgError =  document.querySelector('.error')
+        const containerMsgSucesso =  document.querySelector('.success')
+        containerMsgError.innerHTML = msgError;
+        containerMsgError.style.display = 'block';
+        containerMsgSucesso.style.display = 'none';
+        aValue.value = "";
+        bValue.value = "";
+    }   
+    else{
+        const containerMsgSucesso =  document.querySelector('.success')
+        const containerMsgError =  document.querySelector('.error')
+        containerMsgSucesso.innerHTML = msgSucesso;
+        containerMsgSucesso.style.display = 'block';
+        containerMsgError.style.display = 'none';
+        aValue.value = "";
+        bValue.value = "";
+    };
+})
