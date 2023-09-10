4842c7afbf2c1c925fcff17921bab59d88138c66
Wesley Jakob <lewesleydj@gmail.com>
2023-09-10 16:24:18 -0300

    exercicio de typescript

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplica(numero1:number, numero2:number){
+    return numero1 * numero2;
+}
+
+function saudacao(nome:string){
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao('wesley'));
+console.log(multiplica(10,10));
